eb9f080326311c2fb72468c2321fcec22f8d79fd
John Wardrop <johnwardrop@outlook.com>
2019-05-12 15:00:38 +0100

    Initial

---
+++ dscconfig/cishardentest.ps1
@@ -0,0 +1,23 @@
+﻿Configuration CISHardenTest 
+{
+    Param
+    (
+        
+        [String]$SystemTimeZone="UTC"
+      
+    )
+
+    Import-DscResource -ModuleName ComputerManagementDSC
+    Import-DscResource -ModuleName NetworkingDSC
+    Import-DscResource -ModuleName AuditPolicyDSC
+    Import-DscResource -ModuleName SecurityPolicyDSC
+    Import-DscResource –ModuleName PSDesiredStateConfiguration
+    Node ProdNode {
+
+        TimeZone SysTimeZone
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone = $SystemTimeZone
+        }
+    }
+}
---
+++ replace.tf
@@ -0,0 +1,19 @@
+
+data "local_file" "dscfile" {
+    filename = "./dscconfig/cishardentest.ps1"
+}
+
+locals {
+  dsccontent = "${data.local_file.dscfile.content}"
+}
+
+locals {
+  utfdsccontent = "${replace(local.dsccontent,"/\\n/","")}"
+  depends_on = ["local.dsccontent"]
+}
+
+
+output "removeutfhead" {
+  value=["local.dsccontent.utfdsccontent"]
+}
+#${data.local_file.dscfile.content}
